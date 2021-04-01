789d5c0bf2e6e6f58466f9352546d723071df59b
unknown <codingsuka@gmail.com>
2021-04-01 15:12:48 +0700

    Implement CRUD - Create, Read, Update and Delete

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 const postsRoute = require('./routes/posts');
+
+app.use(bodyParser.json());
+
 app.use("/posts", postsRoute);
 
-// app.get('/', (req, res) => {
-//     res.send("hello word");
-// });
+app.get('/', (req, res) => {
+    res.send("hello word");
+});
 
 module.exports = app
