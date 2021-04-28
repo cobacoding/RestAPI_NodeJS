9b5c0b47f6952a8858a85e83c407e82932be92b7
suka_coding <codingsuka@gmail.com>
2021-04-28 09:55:22 +0700

    add middleware and eslint

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const helmet = require('helmet')
+const cors = require('cors')
+const rateLimit = require('express-rate-limit')
+const favicon = require('serve-favicon')
+const path = require('path')
+
+const limiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  //   windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 1, // limit each IP to 100 requests per windowMs
+  message: 'Too many accounts created from this IP, please try again after a minute'
+})
 
 const app = express()
+
+app.use(bodyParser.json())
+// Middlewares headers
+// This disables the `referrerPolicy` middleware but keeps the rest.
+app.use(
+  helmet({
+    referrerPolicy: false
+  })
+)
+app.use(cors())
+app.use(limiter) //  apply to all requests
+
+// Serve Favicon
+app.use(favicon(path.join(__dirname, '/public/images', ('favicon.ico'))))
 
 const postsRoute = require('./routes/posts')
 const userRoute = require('./routes/user')
 const commentsRoute = require('./routes/comments')
 const imageRoute = require('./routes/images')
-
-app.use(bodyParser.json())
 
 app.use('/posts', postsRoute)
 app.use('/user', userRoute)
@@ -17,8 +41,8 @@ app.use('/images', imageRoute)
 // get images
 app.use('/uploads', express.static('uploads'))
 
-// app.get('/', (req, res) => {
-//     res.send("hello word");
-// });
+app.get('/', (req, res) => {
+  res.send('hello word')
+})
 
 module.exports = app
